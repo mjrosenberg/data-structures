e5d729cdfd0ad896114f827e71a7d95f55f73bff
mjrosenberg <mjr70@duke.edu>
2020-05-13 11:50:09 -0700

    functional queue implemented and passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  someInstance.curr = 0; //tracks the current numeric key
+  someInstance.start = 0; //tracks the current front of the keys
+  //idk why we wouldn't just use an array with pop/shift
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.curr] = value;
+    someInstance.curr ++;
   };
 
   someInstance.dequeue = function() {
+    var temp = storage[someInstance.start];
+    storage[someInstance.start] = undefined;
+    someInstance.start ++;
+    return temp;
   };
 
   someInstance.size = function() {
+    return Math.max(someInstance.curr - someInstance.start, 0);
   };
 
   return someInstance;
