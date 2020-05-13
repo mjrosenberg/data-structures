1bbad54979b0474f235a041084a2c02a18e011d2
mjrosenberg <mjr70@duke.edu>
2020-05-13 12:14:07 -0700

    functional stack is passing

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,41 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  someInstance.storage = storage;
+  someInstance.length = 0; //tracks the number of elements in the stack
   someInstance.push = function(value) {
+    if (someInstance.length === 0){
+      someInstance.length+= 1;
+      storage[0] = value;
+    } else{
+      for (i = someInstance.length; i >= 0; i--){
+        var temp = storage[i];
+        storage[i+1] = temp;
+      }
+      someInstance.length+= 1;
+      storage[0] = value;
+    }
   };
 
   someInstance.pop = function() {
+    var temp = storage[0];
+    for (i = 0; i < someInstance.length; i++){
+      storage[i] = storage[i+1];
+    }
+    storage[someInstance.length] = undefined;
+    someInstance.length = someInstance.length -1;
+    return temp;
   };
 
   someInstance.size = function() {
+    return Math.max(someInstance.length,0);
   };
-
+  //console.log(someInstance);
+  //console.log(storage);
   return someInstance;
 };
+
+var s = Stack();
+s.push('a');
+s.push('b');
+console.log(s);
