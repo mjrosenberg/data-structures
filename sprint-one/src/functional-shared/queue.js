8b37c99e4c71c614c1bdd89ba0e9c9ec53a59b4b
mjrosenberg <mjr70@duke.edu>
2020-05-13 14:07:17 -0700

    mostly passing for stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new style
+  var someInstance = {};
+  var storage = {};
+
+  //someInstance.enqueue = this
+  someInstance.storage = storage;
+  someInstance.curr = 0; //tracks the current numeric key
+  someInstance.start = 0; //tracks the current front of the keys
+  // someInstance.pop = queueMethods.push(this);
+  // someInstance.push = queueMethods.pop;
+  //someInstance.size;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
-
+queueMethods.enqueue = function(value){
+  this.storage[this.curr] = value;
+  this.curr ++;
+};
+queueMethods.dequeue = function(){
+  var temp = this.storage[this.start];
+  this.storage[this.start] = undefined;
+  this.start ++;
+  return temp;
+}
+queueMethods.size = function(){
+  return Math.max(this.curr - this.start, 0);
+}
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,44 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+
+  //someInstance.enqueue = this
+  someInstance.storage = storage;
+  someInstance.length = 0; //tracks the current numeric key
+  //someInstance.start = 0; //tracks the current front of the keys
+  // someInstance.pop = queueMethods.push(this);
+  // someInstance.push = queueMethods.pop;
+  //someInstance.size;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
-
+stackMethods.push = function(value){
+  if (this.length === 0){
+    this.length+= 1;
+    this.storage[0] = value;
+  } else{
+    for (i = this.length; i >= 0; i--){
+      var temp = this.storage[i];
+      this.storage[i+1] = temp;
+    }
+    this.length+= 1;
+    this.storage[0] = value;
+  }
+};
+stackMethods.pop = function(){
+  var temp = this.storage[0];
+    for (i = 0; i < this.length; i++){
+      this.storage[i] = this.storage[i+1];
+    }
+    this.storage[this.length] = undefined;
+    this.length = this.length -1;
+    return temp;
+}
+stackMethods.size = function(){
+  return Math.max(this.length, 0);
+}
